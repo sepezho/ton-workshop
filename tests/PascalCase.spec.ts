e715e2ba6e638e1771d410cafbb03281218d3f71
Mark Okhman <mark@okhmans.pro>
2023-02-11 17:01:02 +0100

    tests

--- tests/PascalCase.spec.ts
+++ tests/PascalCase.spec.ts
@@ -1,4 +1,4 @@
-import { Blockchain } from '@ton-community/sandbox';
+import { Blockchain, OpenedContract } from '@ton-community/sandbox';
 import { Cell, toNano } from 'ton-core';
 import { PascalCase } from '../wrappers/PascalCase';
 import '@ton-community/test-utils';
@@ -7,14 +7,36 @@ import { compile } from '@ton-community/blueprint';
 describe('PascalCase', () => {
     let code: Cell;
 
+    let pascalCase: OpenedContract<PascalCase>;
+
     beforeAll(async () => {
         code = await compile('PascalCase');
+    });
+
+    beforeEach(async () => {
+        const blockchain = await Blockchain.create();
+
+        pascalCase = blockchain.openContract(
+            PascalCase.createFromConfig(
+                {
+                    value: 42,
+                },
+                code
+            )
+        );
     });
 
     it('should deploy', async () => {
         const blockchain = await Blockchain.create();
 
-        const pascalCase = blockchain.openContract(PascalCase.createFromConfig({}, code));
+        const pascalCase = blockchain.openContract(
+            PascalCase.createFromConfig(
+                {
+                    value: 42,
+                },
+                code
+            )
+        );
 
         const deployer = await blockchain.treasury('deployer');
 
@@ -25,5 +47,47 @@ describe('PascalCase', () => {
             to: pascalCase.address,
             deploy: true,
         });
+    });
+
+    it('should get default initial value from the storage', async () => {
+        const blockchain = await Blockchain.create();
+
+        const pascalCase = blockchain.openContract(
+            PascalCase.createFromConfig(
+                {
+                    value: 42,
+                },
+                code
+            )
+        );
+
+        const deployerWallet = await blockchain.treasury('deployer');
+
+        const deployResult = await pascalCase.sendDeploy(deployerWallet.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployerWallet.address,
+            to: pascalCase.address,
+            deploy: true,
+        });
+
+        const data = await pascalCase.getData();
+
+        expect(data.value).toEqual(42);
+
+        const increaseResult = await pascalCase.sendIncrease(deployerWallet.getSender(), toNano('0.05'), 1);
+
+        expect(increaseResult.transactions).toHaveTransaction({
+            from: deployerWallet.address,
+            to: pascalCase.address,
+            value: toNano('0.05'),
+            success: true,
+        });
+
+        const newData = await pascalCase.getData();
+
+        expect(newData.value).toEqual(43);
+
+        // console.log(increaseResult.transactions);
     });
 });
--- wrappers/PascalCase.ts
+++ wrappers/PascalCase.ts
@@ -1,9 +1,11 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from 'ton-core';
 
-export type PascalCaseConfig = {};
+export type PascalCaseConfig = {
+    value: number;
+};
 
 export function pascalCaseConfigToCell(config: PascalCaseConfig): Cell {
-    return beginCell().endCell();
+    return beginCell().storeUint(config.value, 64).endCell();
 }
 
 export class PascalCase implements Contract {
@@ -14,7 +16,7 @@ export class PascalCase implements Contract {
     }
 
     static createFromConfig(config: PascalCaseConfig, code: Cell, workchain = 0) {
-        const data = pascalCaseConfigToCell(config);
+        const data = beginCell().storeUint(config.value, 64).endCell();
         const init = { code, data };
         return new PascalCase(contractAddress(workchain, init), init);
     }
@@ -24,6 +26,23 @@ export class PascalCase implements Contract {
             value,
             sendMode: SendMode.PAY_GAS_SEPARATLY,
             body: beginCell().endCell(),
+        });
+    }
+
+    async getData(provider: ContractProvider): Promise<PascalCaseConfig> {
+        const { stack } = await provider.get('get_number', []);
+        return {
+            value: stack.readNumber(),
+        };
+    }
+
+    async sendIncrease(provider: ContractProvider, via: Sender, msg_value: bigint, increase_value: number) {
+        const in_msg_body = beginCell().storeUint(1, 32).storeUint(0, 64).storeUint(increase_value, 64).endCell();
+
+        await provider.internal(via, {
+            value: msg_value,
+            sendMode: SendMode.PAY_GAS_SEPARATLY,
+            body: in_msg_body,
         });
     }
 }
